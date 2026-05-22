9aef46cdf5ca4c9bce44bc0007ebf7bf4713a9a7
Rajan Mahato <rajancdy14@gmail.com>
2026-05-22 19:24:58 +0545

    Delete Tip Calculator directory

--- Tip Calculator/script.js
+++
@@ -1,9 +0,0 @@
-const billamount=document.getElementById("billamount");
-const tipamount=document.getElementById("percentageamount");
-const resultlbl=document.getElementById("resultlbl");
-function calculate(){
-    let bill=Number(billamount.value);
-    let tip=Number(tipamount.value);
-    let total=bill+tip;
-    resultlbl.innerHTML=`Total: <b>$${total}<b>`
-}
